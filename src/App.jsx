1e32165997bf684cb69ab937fd6f30f6c4f2ffa3
Leevi Ilmari Puntanen <leevi.puntanen2@gmail.com>
2023-11-02 16:14:50 +0200

    6.15

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
-import { useState } from "react";
+import { useEffect } from "react";
 import AnecdoteForm from "./components/AnecdoteForm";
 import AnecdoteList from "./components/AnecdoteList";
 import Filter from "./components/Filter";
 import Notification from "./components/Notification";
+import { useDispatch } from "react-redux";
+import { setAnecdote } from "./reducers/anecdoteReducer";
+import anecdoteService from "./services/anecdotes";
 
 const App = () => {
-  const [filter, setFilter] = useState("");
+  const dispatch = useDispatch();
+  useEffect(() => {
+    anecdoteService
+      .getAll()
+      .then((anecdotes) => dispatch(setAnecdote(anecdotes)));
+  });
+
   return (
     <div>
       <h2>Anecdotes</h2>
-      <Filter setFilter={setFilter} />
+      <Filter />
       <Notification />
-      <AnecdoteForm filter={filter} />
+      <AnecdoteForm />
       <AnecdoteList />
     </div>
   );
