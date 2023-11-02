dd05740d290e6cb17d04e195bb45c96d896d1233
Leevi Ilmari Puntanen <leevi.puntanen2@gmail.com>
2023-11-02 17:53:50 +0200

    Viimeisetkin

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,28 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import AnecdoteForm from "./components/AnecdoteForm";
 import AnecdoteList from "./components/AnecdoteList";
 import Filter from "./components/Filter";
 import Notification from "./components/Notification";
 import { useDispatch } from "react-redux";
-import { initializeAnecdotes } from "./reducers/anecdoteReducer";
+import { setAnecdote } from "./reducers/anecdoteReducer";
+import anecdoteService from "./services/anecdotes";
 
 const App = () => {
+  const [filter, setFilter] = useState("");
+
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(initializeAnecdotes());
+    anecdoteService
+      .getAll()
+      .then((anecdotes) => dispatch(setAnecdote(anecdotes)));
   });
 
   return (
     <div>
       <h2>Anecdotes</h2>
-      <Filter />
+      <Filter setFilter={setFilter} />
       <Notification />
-      <AnecdoteForm />
+      <AnecdoteForm filter={filter} />
       <AnecdoteList />
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,18 @@
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
 import App from "./App";
-import store from "./store";
+import anecdoteReducer from "./reducers/anecdoteReducer";
+import filterReducer from "./reducers/filterReducer";
+import notificationReducer from "./reducers/notificationReducer";
+
+const store = configureStore({
+  reducer: {
+    anecdotes: anecdoteReducer,
+    filter: filterReducer,
+    notification: notificationReducer,
+  },
+});
 
 console.log(store.getState());
 
